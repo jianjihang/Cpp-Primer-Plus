62a0f38124359c906d7fb51605fc1ab19303875d
jianjihang <73734589+jianjihang@users.noreply.github.com>
2020-11-17 10:39:30 +0800

    Add files via upload

---
+++ code/fowl.cpp
@@ -0,0 +1,50 @@
+// fowl.cpp -- auto_ptr a poor choice 
+#include <iostream>
+#include <string>
+#include <memory>
+
+/*
+int main() 
+{
+    using namespace std; 
+    auto_ptr<string> films[5] =
+    {
+        auto_ptr<string> (new string("Fowl Balls")), 
+        auto_ptr<string> (new string("Duck Walks")), 
+        auto_ptr<string> (new string("Chicken Runs")), 
+        auto_ptr<string> (new string("Turkey Errors")), 
+        auto_ptr<string> (new string("Goose Eggs"))
+    };
+    auto_ptr<string> pwin;
+    pwin = films[2]; // films[2] loses ownership
+
+    cout << "The nominees for best avian baseball film are\n"; 
+    for (int i = 0; i < 5; i++)
+        cout << *films[i] << endl;
+    cout << "The winner is " << *pwin << "!\n"; 
+    cin.get();
+    return 0;
+}
+*/
+
+int main() 
+{
+    using namespace std; 
+    shared_ptr<string> films[5] =
+    {
+        shared_ptr<string> (new string("Fowl Balls")), 
+        shared_ptr<string> (new string("Duck Walks")), 
+        shared_ptr<string> (new string("Chicken Runs")), 
+        shared_ptr<string> (new string("Turkey Errors")), 
+        shared_ptr<string> (new string("Goose Eggs"))
+    };
+    shared_ptr<string> pwin;
+    pwin = films[2]; // films[2] loses ownership
+
+    cout << "The nominees for best avian baseball film are\n"; 
+    for (int i = 0; i < 5; i++)
+        cout << *films[i] << endl;
+    cout << "The winner is " << *pwin << "!\n"; 
+    cin.get();
+    return 0;
+}
